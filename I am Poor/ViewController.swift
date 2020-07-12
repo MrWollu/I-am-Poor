749fa0cdb267176819e97ce352abe4b2efc2e25c
mrwollu <michaeldw217@hotmail.com>
2020-07-11 23:36:02 -0500

    Initial Commit

---
+++ I am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Michael Wolljung on 7/11/20.
+//  Copyright © 2020 Michael Wolljung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
